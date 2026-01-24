1f5356d331d5678b64f842ff0058fe38d3583d1c
Holstein-joe <kamekiti41@gmail.com>
2026-01-24 11:34:21 +0900

    feat: link database secret and re-enable prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,11 @@
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: any }
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-export const prisma = {} as any;
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient()
 
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
 
